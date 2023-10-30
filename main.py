cdd2d055495fe5c66a2ab663600a0ffc1f2829ce
tec9na <149070162+tec9na@users.noreply.github.com>
2023-10-30 14:42:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,22 @@ from machine import Pin, UART, PWM, I2C
 import umqtt_robust2 as mqtt
 from neopixel import NeoPixel
 from time import sleep
-import _thread
+#import _thread
 from mpu6050 import MPU6050
 from gps_bare_minimum import GPS_Minimum
+import math
 
 #mqtt.sync_with_adafruitIO()
 #mqtt.webprint(data)
 #mqtt.besked(adafruit_data)
 
 n = 12
-np = NeoPixel(Pin(26, Pin.OUT),n)
+np = NeoPixel(Pin(13, Pin.OUT),n)
 
 
 i2c = I2C(scl=Pin(22),sda=Pin(21))
 imu = MPU6050(i2c)
+vals = imu.get_values()
 
 #########################################################################
 # CONFIGURATION
@@ -32,8 +34,8 @@ def get_adafruit_gps():
         # hvis der er kommet end bruggbar værdi på alle der skal anvendes
         if gps.get_speed() != -999 and gps.get_latitude() != -999.0 and gps.get_longitude() != -999.0 and gps.get_validity() == "A":
             # gemmer returværdier fra metodekald i variabler
-            speed =str(gps.get_speed())
-            lat = str(gps.get_latitude())
+            speed =str(gps.get_speed()) 
+            lat = str(gps.get_latitude()) 
             lon = str(gps.get_longitude())
             # returnerer data med  adafruit gps format
             return speed + "," + lat + "," + lon + "," + "0.0"
@@ -52,7 +54,7 @@ def set_color(tackling_indikator):
 
     
     
-status = False
+status = True #hører til IMU
 
 def clear_neo():
     for i in range(n):
@@ -60,61 +62,77 @@ def clear_neo():
         np.write()
         
 clear_neo()
+
+buzzer = PWM(Pin(26), freq=420, duty=0)
+
+tid = 0
+
 while True:
     try:
-        #IMU koden kommer her:
-        # printer hele dictionary som returneres fra get_values metoden
-        print(imu.get_values()) 
+        #IMU koden kommer her:#############################################################
+        ###### printer hele dictionary som returneres fra get_values metoden, er dog udkommenteret lige nu
+        #print(imu.get_values()) 
         sleep(0.01)
-        vals = imu.get_values()
+        value_acceleration_x = vals["acceleration x"]
+        print(f"ACCELERATION ER: {value_acceleration_x}")
+        #print(vals["acceleration x"])
+            
         #ligge ned kode
-        if vals["acceleration x"] < -5000 and status == False:
-            print("jeg ligger ned")
+        if vals["acceleration x"] < -1000 and status == False:
+            print("spiller er: TACKLET")
             status = True
-            set_color(tackling_indikator)
-            tackling_indikator = tackling_indikator+1
             if tackling_indikator < 10 :
                 
                 set_color(tackling_indikator)
                 tackling_indikator = tackling_indikator+1
         
-        
+        print("")
         #stå op kode:
         if vals["acceleration x"] > 5000 and status == True:
             status = False
-            print("jeg står op")
-        print(status)
-        sleep(1)        
+            print("spiller er: OPREJST")
+        print("")
         
-        
-        
-        
-        
-        #Her kommer GPS koden:
+        #Her kommer GPS koden:##############################################################
         
         gps_data = get_adafruit_gps()
         if gps_data: # hvis der er korrekt data så send til adafruit
             print(f'\ngps_data er: {gps_data}')
             mqtt.web_print(gps_data, 'tec9na/feeds/mapfeed/csv')
         
+
+
         
-        #Her sender vi og modtager data mellem ESP og Adafruit
+        #Her sender vi og modtager data mellem ESP og Adafruit##############################
 
         if len(mqtt.besked) != 0: # Her nulstilles indkommende beskeder
             mqtt.besked = ""            
         mqtt.sync_with_adafruitIO() # igangsæt at sende og modtage data med Adafruit IO             
-        print(".", end = '') # printer et punktum til shell, uden et enter 
-        sleep(2)
         
+        
+        # lav liste med hastighedsmålinger
+        # tilføj nuværende hastighed til listen
+        # lav tæller variabel (15x loop med 2 sek delay = 30 sekunder)
+            # når tælleren er gået så tjek liste med hastighedsmålinger
+             # hvis den højeste hastighed i listen er under 5 km/t så aktiver buzzer
+        
+        print("")
         #selvvalgt krav#####################################################################
+        if gps.get_speed() < 1: #hvis man er inaktiv
+            print("SPILLER ER: INAKTIV")
+            tid = tid + 2   #bruges til at tælle 2 op hver gang loopet kører OG man er inaktiv. Tæller med 2 da vi sleeper på 2 i loopet og vi vil gerne have at den siger en buzzer lyd efter 30 sekunder.
+            if tid >= 30: #hvis der er gået 30 sekunder så spiller buzzeren en lyd
+                buzzer.duty(512) #buzzeren tænder
         
-        if gps.get_speed() > 2:
-            print("aktiv")
-             
-    
+        if gps.get_speed() > 1: #hvis man er aktiv
+            print("SPILLER ER: AKTIV")
+            tid = 0 #timeren nulstilles
+            buzzer.duty(0) #buzzeren stopper med at spille lyd
+            
+                
+                
+        sleep(3)
     except KeyboardInterrupt:
         print("Ctrl+C pressed - exiting program.")
         sys.exit()
-
-
 
